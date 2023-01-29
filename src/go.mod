7bcbf03f321150b2a659b96664c21aa3824c0cd0
Nikurasu <publicmail@nikurasu.gay>
2023-01-29 19:24:15 +0100

    Copy static files for startpage into docker container

--- src/go.mod
+++ src/go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/otiai10/copy v1.9.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go/codec v1.2.8 // indirect
--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/otiai10/copy"
 	"nikurasu.gay/static-hoster/envloader"
 	"nikurasu.gay/static-hoster/router"
 )
@@ -12,7 +13,11 @@ import (
 func main() {
 	var env = envloader.Load()
 	if _, err := os.Stat(fmt.Sprintf("%s404.html", env.StaticDir)); errors.Is(err, os.ErrNotExist) {
-		os.Create(fmt.Sprintf("%s404.html", env.StaticDir))
+		if _, err := os.Stat("/usr/share/static-hoster/html"); errors.Is(err, os.ErrNotExist) {
+			os.Create(fmt.Sprintf("%s404.html", env.StaticDir))
+		} else {
+			copy.Copy("/usr/share/static-hoster/html", env.StaticDir)
+		}
 	}
 	r := router.Create(env)
 	r.Run(env.Port)
