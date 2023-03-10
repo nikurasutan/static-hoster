5899f7f3131bd013087db56aff9a130ccedea9b6
nikurasu <publicmail@nikurasu.gay>
2023-03-10 19:32:54 +0100

    Copy files from embedFS to storage

--- src/main.go
+++ src/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"errors"
+	"embed"
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/otiai10/copy"
 	"nikurasu.gay/static-hoster/envloader"
+	"nikurasu.gay/static-hoster/helper"
 	"nikurasu.gay/static-hoster/router"
 )
 
+//go:embed static/*
+var assets embed.FS
+
 func main() {
 	var env = envloader.Load()
-	if _, err := os.Stat(fmt.Sprintf("%s404.html", env.StaticDir)); errors.Is(err, os.ErrNotExist) {
-		if _, err := os.Stat("/usr/share/static-hoster/html"); errors.Is(err, os.ErrNotExist) {
-			os.Create(fmt.Sprintf("%s404.html", env.StaticDir))
-		} else {
-			copy.Copy("/usr/share/static-hoster/html", env.StaticDir)
-		}
+	if empty, _ := helper.IsEmpty(env.StaticDir); empty {
+		copyEmbed(env.StaticDir)
+		fmt.Println("Created default files")
 	}
+	copyEmbed(env.StaticDir)
 	r := router.Create(env)
 	r.Run(env.Port)
+}
+
+func copyEmbed(dest string) {
+	files, err := assets.ReadDir("static")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range files {
+		fileContent, err := assets.ReadFile(fmt.Sprintf("static/%s", file.Name()))
+		if err != nil {
+			fmt.Println(err)
+		}
+		fileName := fmt.Sprintf("%s%s", dest, file.Name())
+		if err := os.WriteFile(fileName, fileContent, 0666); err != nil {
+			log.Printf("Error writing default Files: %s", err)
+		}
+	}
 }
